9c287d853e915896428085e4a0ccbb452c710989
Anass LAHNIN <anass.lahnin@g4.fr>
2024-03-04 18:06:07 +0100

    More logging, increased pool size

--- src/functions/ingest.js
+++ src/functions/ingest.js
@@ -29,14 +29,18 @@ app.http("ingest", {
     context.log(`Http function processed request for url "${request.url}"`);
 
     const config = {
-      host: process.env["PGHOST"],
-      port: process.env["PGPORT"],
-      user: process.env["PGUSER"],
-      password: process.env["PGPASSWORD"],
-      database: process.env["PGDATABASE"],
-      ssl: true,
-      // idleTimeoutMillis: 1000,
-      allowExitOnIdle: true,
+      host: process.env.AZURE_POSTGRESQL_HOST,
+      port: Number(process.env.AZURE_POSTGRESQL_PORT),
+      user: process.env.AZURE_POSTGRESQL_USER,
+      password: process.env.AZURE_POSTGRESQL_PASSWORD,
+      database: process.env.AZURE_POSTGRESQL_DATABASE,
+      ssl: process.env.AZURE_POSTGRESQL_SSL,
+      max: 30,
+      application_name: 'azure_functions',
+      // log: (...args) => { context.trace(...args); },
+      // idleTimeoutMillis: 0, // disable disconnect of idle client
+      // statement_timeout: 10000, // 10s
+      // allowExitOnIdle: true,
     };
 
     const tableName = request.query.get("table");
@@ -51,8 +55,14 @@ app.http("ingest", {
       const body = await request.json();
       const queries = createQuery(body, tableName);
 
+      const size = Buffer.byteLength(JSON.stringify(body));
+      context.log(`Received ${size / 1024 / 1024} MB of data (${queries.length} rows)`);
+
       // Connect to the database
       const pool = new pg.Pool(config);
+      pool.on('error', (err, client) => {
+        context.error('backend error', err);
+      });
 
       // Prepare queries - ignore errors and proceeed
       const startTime = Date.now();
@@ -62,7 +72,8 @@ app.http("ingest", {
       const rejected = results
         .map((result, idx) => ({ row: idx + 1, result }))
         .filter((result) => result.result.status === "rejected");
-      context.log(`Done, ${results.length - rejected.length} rows processed in ${Date.now() - startTime} ms`);
+      
+      context.log(`Done, ${results.length - rejected.length} rows (${size / 1024 / 1024} MB) processed in ${Date.now() - startTime} ms`);
 
       const messages = rejected.map((rej) => ({
         row: rej.row,
@@ -71,7 +82,7 @@ app.http("ingest", {
 
       await pool.end();
       if (rejected.length > 0) {
-        context.error(messages);
+        context.error(JSON.stringify(rejected));
         return { status: 206, jsonBody: messages };
       }
 
