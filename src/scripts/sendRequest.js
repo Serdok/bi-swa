7020526034f49c9e01bb314817e434a941d62574
Anass LAHNIN <anass.lahnin@g4.fr>
2024-03-06 17:44:42 +0100

    fix syntax errors

--- src/scripts/sendRequest.js
+++ src/scripts/sendRequest.js
@@ -1,7 +1,7 @@
 import { createReadStream } from 'fs';
 
 async function execute_request() {
-    const stream = createReadStream('./lines.ndjson')
+    const stream = createReadStream('./data.json')
 
     await fetch('http://127.0.0.1:7071/api/ingest?table=tld_bi33_margins_report', {
         method: 'POST',
