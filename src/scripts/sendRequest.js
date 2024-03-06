1d7ae2729d4b3b3683c355b70568610100386ffb
Anass LAHNIN <anass.lahnin@g4.fr>
2024-03-06 17:49:39 +0100

    target endpoint

--- src/scripts/sendRequest.js
+++ src/scripts/sendRequest.js
@@ -3,7 +3,7 @@ import { createReadStream } from 'fs';
 async function execute_request() {
     const stream = createReadStream('./data.json')
 
-    await fetch('http://127.0.0.1:7071/api/ingest?table=tld_bi33_margins_report', {
+    await fetch('https://poc-bi-api.azurewebsites.net/api/ingest?table=tld_bi33_margins_report', {
         method: 'POST',
         body: stream,
         duplex: 'half',
